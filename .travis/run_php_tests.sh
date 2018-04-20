402cb50d12ae57c39cbdcc07f3f0a76da0a28319
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2018-04-21 00:18:10 +0300

    Run tests under Valgrind

--- .travis/run_php_tests.sh
+++ .travis/run_php_tests.sh
@@ -5,7 +5,7 @@ DIR=$(readlink -enq "$(dirname $0)/../")
 shopt -s nullglob
 export NO_INTERACTION=1
 export REPORT_EXIT_STATUS=1
-make -C "$DIR" test
+make -C "$DIR" TESTS=-m test
 for i in $DIR/tests/*.log $DIR/tests/*.mem; do
 	echo "====== $i ======";
 	cat "$i";
